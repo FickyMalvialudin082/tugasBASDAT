e207acfc5fcf891e1ffc88504ded1c0e52b31653
Ficky Malvialudin <2406082@itg.ac.id>
2026-06-23 16:00:47 +0700

    update

--- hewan.php
+++ hewan.php
@@ -203,7 +203,7 @@ function confirmHapus(event, nama) {
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
+        cancelButtonColor: '#0a261f',
         confirmButtonText: 'Ya, hapus!',
         cancelButtonText: 'Batal'
     }).then((result) => {
